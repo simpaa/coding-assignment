992b0b379b4808813a99e19d48846fb2d8add10c
Simon Andersson <simpaa11@gmail.com>
2022-05-17 21:24:31 +0200

    Cleaned up some boilerplate code and added collague page

---
+++ src/pages/colleagues.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from '../components/Heading';
+
+const Colleagues: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <Heading text="The fellowship of the 13|37" />
+      </header>
+    </div>
+  );
+}
+
+export default Colleagues;
